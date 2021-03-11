5d6a64903a85e0edb78c6809481e8c027b8916e2
Xyneiz <wllmdd@gmail.com>
2021-03-11 14:41:19 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import "./styles.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Tasklist from "./Components/Tasklist/Tasklist";
+
+let idAcc = 0;
+const generateId = () => {
+  idAcc = idAcc + 1;
+  return idAcc;
+};
+
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (title, state) => {
+    const newTask = {
+      id: generateId(),
+      title,
+      state
+    };
+
+    setTasks((existingTasks) => {
+      return [...existingTasks, newTask];
+    });
+  };
+
+  const updateTask = (id, title, state) => {
+    setTasks((existingTasks) => {
+      return existingTasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, title, state };
+        } else {
+          return task;
+        }
+      });
+    });
+  };
+
+  const deleteTask = (id) => {
+    setTasks((existingTasks) => {
+      return existingTasks.filter((task) => task.id !== id);
+    });
+  };
+
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="container">
+        <Tasklist
+          title="Pendente"
+          onAddTask={addTask}
+          tasks={tasks.filter((t) => t.state === "Pendente")}
+          taskState="Pendente"
+          onTaskUpdate={updateTask}
+          onDeleteTask={deleteTask}
+        />
+        <Tasklist
+          title="Fazendo"
+          onAddTask={addTask}
+          tasks={tasks.filter((t) => t.state === "Fazendo")}
+          taskState="Fazendo"
+          onTaskUpdate={updateTask}
+          onDeleteTask={deleteTask}
+        />
+        <Tasklist
+          title="Completa"
+          onAddTask={addTask}
+          tasks={tasks.filter((t) => t.state === "Completa")}
+          taskState="Completa"
+          onTaskUpdate={updateTask}
+          onDeleteTask={deleteTask}
+        />
+      </div>
+    </div>
+  );
+}
